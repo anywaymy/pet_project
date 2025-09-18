c9fb6d0a0743d658faa22038c4f12cb8306d43e2
anywaymy <simpletop4ik@gmail.com>
2025-09-18 14:54:47 +0300

    registration created

--- some_kind/users/forms.py
+++ some_kind/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -17,3 +17,28 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
+class UserRegistrationForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Username',
+        'class': 'input-content__input'
+    }))
+
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Email',
+        'class': 'input-content__input'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Введите пароль',
+        'class': 'input-content__input'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Повторите пароль',
+        'class': 'input-content__input'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
